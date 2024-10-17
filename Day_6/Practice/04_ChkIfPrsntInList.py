1c1933dbdd9671d7183ae02d05bab4bcf467da84
Muhammad Jamshaid <mjamshaidijaz786@gmail.com>
2024-10-17 22:48:16 +0500

    exercise

---
+++ Day_6/Practice/04_ChkIfPrsntInList.py
@@ -0,0 +1,7 @@
+# write a program which finds out whether a given name is present in a list or not.
+l = ['ali','haidar','bilal','azeem',]
+name = input("Enter name :")
+if(name in l):
+    print("name is persent in the list")
+else:
+    print(("name is not present in list "))
