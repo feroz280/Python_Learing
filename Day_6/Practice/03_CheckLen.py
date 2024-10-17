df49bcba0fe7f23ee23eff1c84e43fe972537775
Muhammad Jamshaid <mjamshaidijaz786@gmail.com>
2024-10-17 22:25:00 +0500

    Day_6 Exercise_03

---
+++ Day_6/Practice/03_CheckLen.py
@@ -0,0 +1,7 @@
+n = input("Enter user name :")
+if(len(n)>10):
+    print("username contain more than 10 characters")
+elif(len(n)==10):
+    print("username conntain 10 characters ")
+else:
+    print("username contain less than 10 characters")
